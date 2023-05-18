f5e4de801fc2e5660d6646e5308b31b2162ab8b1
Darknab <darknab@live.fr>
2023-05-18 14:16:07 +0100

    Add a config.host to try to resolve the -application failed to respond- problem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Added to try to fix the "Application fails to respond problem"
+  config.hosts << "https://blog-production-51b2.up.railway.app/"
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
