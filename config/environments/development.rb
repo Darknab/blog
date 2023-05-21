1820a6463ecb8dd655ca6b0d328e54be8f83221a
Darknab <darknab@live.fr>
2023-05-21 15:03:27 +0100

    Revert "Add a config.host to try to resolve the -application failed to respond- problem"
    This reverts commit f5e4de801fc2e5660d6646e5308b31b2162ab8b1.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,10 +3,6 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-
-  # Added to try to fix the "Application fails to respond problem"
-  config.hosts << "https://blog-production-51b2.up.railway.app/"
-  
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
